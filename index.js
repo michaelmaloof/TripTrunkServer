b893f1d50bd6ac2c62b03193f5069c25973a3a14
Archon Light <mike@altri.us>
2016-11-23 11:34:22 -0500

    update settings to use simple mailgun for password reset

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const S3Adapter = require('parse-server').S3Adapter;
 const path = require('path');
 
 const config = require('./config');
+// var SimpleMailgunAdapter = require('parse-server/lib/Adapters/Email/SimpleMailgunAdapter');
 
 // let logger = require('parse-server/lib/Adapters/Logger/FileLoggerAdapter').FileLoggerAdapter;
 const api = new ParseServer({
@@ -21,6 +22,16 @@ const api = new ParseServer({
     'triptrunk',
     { directAccess: true }
   ),
+    appName: 'TripTrunk',
+    publicServerURL: 'https://api-dev.triptrunkapp.com/parse/',
+    emailAdapter: {
+        module: 'parse-server-simple-mailgun-adapter',
+            options: {
+                fromAddress: 'noreply@triptrunkapp.com',
+                apiKey: 'key-612b759d61c51fcb92c4cdbe10b36d2e',
+                domain: 'sandbox259dcac5ce094ffcbce8542ba22fda37.mailgun.org',
+            }
+    },
   push: {
     ios: [
       {
